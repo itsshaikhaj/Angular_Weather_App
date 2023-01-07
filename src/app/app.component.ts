e63ae028642d1546a46c6acb7ed37d08ae3a1f8a
itsshaikhaj <95071932+itsshaikhaj@users.noreply.github.com>
2023-01-07 13:27:40 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,14 +8,14 @@ import { takeUntil, map } from 'rxjs/operators';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
   stateInfo: any = [];
   countryInfo: any[] = [];
   cityInfo: any = [];
 
-  constructor(private router: Router, private country: CountriesService) { }
+  constructor(private router: Router, private country: CountriesService) {}
 
   ngOnInit() {
     this.getCountries();
@@ -38,39 +38,38 @@ export class AppComponent implements OnInit {
   //   this.unsubscribe$.complete();
   // }
 
-  getCountries(){
-    this.country.allCountries().
-    subscribe(
-      data2 => {
-        this.countryInfo=data2.Countries;
+  getCountries() {
+    this.country.allCountries().subscribe(
+      (data2) => {
+        this.countryInfo = data2.Countries;
         console.log('this.countryInfo :', this.countryInfo);
         //console.log('Data:', this.countryInfo);
       },
-      err => console.log(err),
+      (err) => console.log(err),
       () => console.log('complete')
-    )
+    );
   }
 
   onChangeCountry(countryValue) {
-  console.log(' $$$$$$$$$$$:', );
-  console.log('countryValue :', countryValue);
-    this.stateInfo=this.countryInfo[countryValue].States;
+    console.log(' $$$$$$$$$$$:');
+    console.log('countryValue :', countryValue);
+    this.stateInfo = this.countryInfo[countryValue].States;
     console.log('this.stateInfo :', this.stateInfo);
-    this.cityInfo=this.stateInfo[0].Cities;
+    this.cityInfo = this.stateInfo[0].Cities;
     console.log(this.cityInfo);
   }
 
   onChangeState(stateValue) {
-    this.cityInfo=this.stateInfo[stateValue].Cities;
+    this.cityInfo = this.stateInfo[stateValue].Cities;
     console.log('this.cityInfo :', this.cityInfo);
     //console.log(this.cityInfo);
   }
 
   onChangeCity(city) {
-  console.log('city :', city);
+    console.log('city :', city);
     const currentUrl = this.router.url;
-    this.router.navigateByUrl('/', {skipLocationChange: true}).then(() => {
-        this.router.navigate([city]);
+    this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
+      this.router.navigate([city]);
     });
   }
 }
